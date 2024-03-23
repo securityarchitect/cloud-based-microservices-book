2d11d46d3fd57054f67bef46b28d296b868b0916
Arshdeep Bahga <abm@merklemail.com>
2024-03-23 12:48:14 +0530

    Add files via upload

---
+++ microservices-ecommerce-ec2/frontend/readme.txt
@@ -0,0 +1,36 @@
+#Install the required packages 
+sudo apt update
+sudo apt install python3-pip python3-dev nginx supervisor
+sudo apt install libcairo-5c-dev
+
+#Install the python packages
+sudo pip3 install -r requirements.txt
+
+#Setup the DB
+flask db init
+flask db migrate
+flask db upgrade
+
+#Run the app
+python3 app.py 
+
+#Setup Nginx
+sudo cp nginx-flask.conf /etc/nginx/sites-available/
+cd /etc/nginx/sites-enabled/
+sudo ln -s ../sites-available/nginx-flask.conf .
+sudo service nginx restart
+
+#Setup Supervisor
+sudo cp frontend-supervisor.conf /etc/supervisor/conf.d/
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl restart all
+
+#Getting SSL Certificate
+sudo apt install certbot
+sudo service nginx stop
+sudo certbot certonly --standalone -d app.study411.com
+
+#Create products (replace localhost with private IP of product service)
+
+curl -X POST -F "name=Coffee Mug" -F "slug=mug-1" -F "description=These premium quality coffee mugs are crafted from durable, dishwasher-safe ceramic and feature stunning, unique designs that add a touch of style to your morning routine. With a generous 300ml capacity and comfortable, ergonomic handle, you can enjoy more of your favorite hot beverage with each sip. The mugs have a glossy finish that looks elegant and feels smooth to the touch. Microwave safe for convenient reheating, they make for thoughtful, personalized gifts for birthdays, holidays, anniversaries, or to show appreciation for someone special. Arriving beautifully packaged in a secure box, these eye-catching mugs are ready to delight any recipient." -F "price=9.99" -F "image=@mug.jpg" http://localhost:5000/api/product/create
---
+++ microservices-ecommerce-ec2/user/app.py
@@ -0,0 +1,218 @@
+import os
+from flask import Flask, send_from_directory, send_file
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask.sessions import SecureCookieSessionInterface
+from flask import g
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_login import UserMixin
+from passlib.hash import sha256_crypt
+from flask import make_response, request, jsonify
+from flask_login import current_user, login_user, logout_user, login_required
+from passlib.hash import sha256_crypt
+from flask import g
+from flask.sessions import SecureCookieSessionInterface
+from flask_login import user_loaded_from_header
+import time, random
+import avinit
+import imghdr
+import base64
+
+class CustomSessionInterface(SecureCookieSessionInterface):
+    """Prevent creating session from API requests."""
+    def save_session(self, *args, **kwargs):
+        if g.get('login_via_header'):
+            return
+        return super(CustomSessionInterface, self).save_session(*args, **kwargs)
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "DoWgTDq87Kmne3TsCjNFabP"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['ENV'] = "development"
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///' + os.path.join(basedir, 'user.sqlite')
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+db.init_app(app)
+login_manager.init_app(app)
+
+migrate = Migrate(app, db)
+app.session_interface = CustomSessionInterface()
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    first_name = db.Column(db.String(255), unique=False, nullable=True)
+    last_name = db.Column(db.String(255), unique=False, nullable=True)
+    password = db.Column(db.String(255), unique=False, nullable=False)
+    photo = db.Column(db.String(255), unique=False, nullable=True)
+    is_admin = db.Column(db.Boolean, default=False)
+    authenticated = db.Column(db.Boolean, default=False)
+    api_key = db.Column(db.String(255), unique=True, nullable=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    date_updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
+
+    def encode_api_key(self):
+        self.api_key = sha256_crypt.hash(self.username + str(datetime.utcnow))
+
+    def encode_password(self):
+        self.password = sha256_crypt.hash(self.password)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+    def to_json(self):
+        img_path = 'uploads/'+self.photo
+        img_type = imghdr.what(img_path)
+        with open(img_path, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+        if img_type == 'jpeg':
+            prefix = 'data:image/jpeg;base64,'
+        elif img_type == 'png':
+            prefix = 'data:image/png;base64,' 
+        img_data = prefix + encoded_string.decode('utf-8') 
+
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'username': self.username,
+            'email': self.email,
+            'photo': img_data,
+            'id': self.id,
+            'api_key': self.api_key,
+            'is_active': True,
+            'is_admin': self.is_admin
+        }
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=user_id).first()
+
+
+@login_manager.request_loader
+def load_user_from_request(request):
+    api_key = request.headers.get('Authorization')
+    if api_key:
+        api_key = api_key.replace('Basic ', '', 1)
+        user = User.query.filter_by(api_key=api_key).first()
+        if user:
+            return user
+    return None
+
+
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    data = []
+    for row in User.query.all():
+        data.append(row.to_json())
+
+    response = jsonify(data)
+    return response
+
+@app.route('/api/user/create', methods=['POST'])
+def post_register():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    username = request.form['username']
+
+    password = sha256_crypt.hash((str(request.form['password'])))
+
+    user = User()
+    user.email = email
+    user.first_name = first_name
+    user.last_name = last_name
+    user.password = password
+    user.username = username
+    user.authenticated = True
+
+
+    name=first_name+' '+ last_name
+    filename = "user"+str(int(time.time()*1000))+".png"
+    imgpath=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    r = lambda: random.randint(0,255)
+    colors=[]
+    colors.append('#%02X%02X%02X' % (r(),r(),r()))
+    avinit.get_png_avatar(name, output_file=imgpath, colors=colors)
+    user.photo = filename
+
+    db.session.add(user)
+    db.session.commit()
+
+    response = jsonify({'message': 'User added', 'result': user.to_json()})
+
+    return response
+
+
+@app.route('/api/user/login', methods=['POST'])
+def post_login():
+    username = request.form['username']
+    user = User.query.filter_by(username=username).first()
+    if user:
+        if sha256_crypt.verify(str(request.form['password']), user.password):
+            user.encode_api_key()
+            db.session.commit()
+            login_user(user)
+
+            return make_response(jsonify({'message': 'Logged in', 'api_key': user.api_key}))
+
+    return make_response(jsonify({'message': 'Not logged in'}), 401)
+
+
+@app.route('/api/user/logout', methods=['POST'])
+def post_logout():
+    if current_user.is_authenticated:
+        logout_user()
+        return make_response(jsonify({'message': 'You are logged out'}))
+    return make_response(jsonify({'message': 'You are not logged in'}))
+
+
+@app.route('/api/user/<username>/exists', methods=['GET'])
+def get_username(username):
+    item = User.query.filter_by(username=username).first()
+    if item is not None:
+        response = jsonify({'result': True})
+    else:
+        response = jsonify({'message': 'Cannot find username'}), 404
+    return response
+
+
+@login_required
+@app.route('/api/user', methods=['GET'])
+def get_user():
+    if current_user.is_authenticated:
+        return make_response(jsonify({'result': current_user.to_json()}))
+
+    return make_response(jsonify({'message': 'Not logged in'})), 401        
+
+@app.route('/api/userid/<id>', methods=['GET'])
+def userid(id):
+    item = User.query.filter_by(id=id).first()
+    if item is not None:
+        response = jsonify({'result': item.to_json()})
+    else:
+        response = jsonify({'message': 'Cannot find user'}), 404
+    return response
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@user_loaded_from_header.connect
+def user_loaded_from_header(self, user=None):
+    g.login_via_header = True
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
